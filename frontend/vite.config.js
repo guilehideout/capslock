289bcc7d02bec222e5158cc83367576cce59df62
Shobhan-Patra <230170146055@vgecg.ac.in>
2025-08-30 02:00:41 +0530

    Add PWA functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#16a34a',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: '/assets/react.svg',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: './assets/react.svg',
             sizes: '512x512',
             type: 'image/png'
           }
